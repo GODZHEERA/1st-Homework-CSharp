6d288ed06a59f41d3fec37adfe157488cfaf3a5e
GODZHEERA <volkins@mail.ru>
2022-06-09 18:10:20 +0300

    Task 1 completed

---
+++ Task_1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число а: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число б: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine("Число а - большее");
+}
+
+else if (a==b)
+{
+    Console.WriteLine("Числа равны");
+}
+
+else
+    {
+        Console.WriteLine("Число б - большее");
+    }
