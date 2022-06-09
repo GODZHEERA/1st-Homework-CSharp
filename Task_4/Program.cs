71c6095a3bef647f5d9b570efb72b48a6be88737
GODZHEERA <volkins@mail.ru>
2022-06-09 20:31:29 +0300

    Task 4 complete

---
+++ Task_4/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+int min = 2;
+
+
+/* if (a%2 == 0){
+    Console.WriteLine(min);
+    min = (min++);
+}
+else
+{
+    min = (min++);
+} */
+
+if (a > 1) {
+   do {
+    Console.Write(min + ", ");
+    min = min + 2;
+    } while (min <= a);
+}
+else {
+    Console.WriteLine("Слишком маленькое число");
+}
